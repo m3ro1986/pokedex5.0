b7b296b67122f64ac178558193b8598aa0414f43
m3ro1986 <m3ro_1986@outlook.com>
2023-02-09 14:11:35 -0600

    ajustesLogin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,15 @@
 import '../styles/login.css';
 import buttonsvg from '../assets/buttonsvg.png';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
     const navigate = useNavigate();
-    const [ theName, setTheName ] = useState('');
+    const [theName, setTheName] = useState('');
+    const [message, setMessage] = useState('')
+    const [ count, setCount ] = useState(0);
+    const [ letter, setLetter ] = useState('');
 
     return (
         <div className='loginBox'>
@@ -19,25 +23,29 @@ const Login = () => {
                     </div>
                 </div>
                 <div className='loginBox-main'>
-                    <div className='loginBox-main_welcome'> 
+                    <div className='loginBox-main_welcome'>
                         <div className='ligth-small'>
                             <div></div>
                             <div></div>
                         </div>
                         <div className='loginBox-main_message'>
-                            <p style={{ margin: '10px'}}>Hello trainer!!! give me your name to start?</p>
+                            <p style={{ margin: '10px' }} className='welcome-message'> 
+                                <span>Hello trainer!</span>
+                                <span>Give me your</span>
+                                <span>name.</span> 
+                            </p>
                             <input
-                                 type="text"
-                                 value={ theName }
-                                 onChange={ e => setTheName( e.target.value )}
+                                type="text"
+                                value={theName}
+                                onChange={e => setTheName(e.target.value)}
                             />
                         </div>
                         <div className='button-box'>
-                            <button onClick={ () => navigate('/pokedex')}> Go </button>
-                            <img src={ buttonsvg } alt="" style={{ width:90 }}/>
+                            <button onClick={() => navigate('/pokedex')}> Go </button>
+                            <img src={buttonsvg} alt="" style={{ width: 90 }} />
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
