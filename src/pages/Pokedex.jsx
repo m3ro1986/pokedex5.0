a97ea5b4c1db02b0a389f9992e1fca5c0428f3a5
m3ro1986 <m3ro_1986@outlook.com>
2023-02-09 21:16:20 -0600

    pokedex

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,5 +1,5 @@
 import '../styles/pokedex.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Pokecard from '../components/Pokecard';
 
@@ -7,13 +7,21 @@ const Pokedex = () => {
 
     const userName = useSelector( state => state.userName );
     const allPokemons = useSelector( state => state.allPokemons );
+    const [ reveal, setReveal ] = useState('hide')
 
-    console.log( allPokemons )
+    const showSide = () => {
+        if ( reveal === 'hide') {
+            setReveal('show')
+        } else {
+            setReveal('hide')
+        }
+    }
 
     return (
         <div className='pokedex-box ligth'>
             <nav>
                 <p> Welcome { userName }, here you can find your favorite POKEMON!!!</p>
+                <i onClick={ showSide } className='bx bxs-left-arrow'></i>
             </nav>
             <main>
                 {
@@ -22,6 +30,9 @@ const Pokedex = () => {
                     ))
                 }
             </main>
+            <div className={`sideBox ${ reveal }`}>
+                <i onClick={ showSide } class='bx bx-x'></i>
+            </div>
         </div>
     );
 };
